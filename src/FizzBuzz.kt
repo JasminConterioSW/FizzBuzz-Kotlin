16f82658cdb0de7c6752a65d9f862096192a820c
JasCon <jasmin.conterio@softwire.com>
2024-09-09 14:57:56 +0100

    FizzBuzz add Bang and Bong

--- src/FizzBuzz.kt
+++ src/FizzBuzz.kt
@@ -2,8 +2,11 @@ fun main() {
     for (i in 1..100) {
         var output = buildString {
             if (i%3 == 0) { append("Fizz") }
-            if (i%5 == 0) {append("Buzz") }
+            if (i%5 == 0) { append("Buzz") }
+            if (i%5 == 0) { append("Bang") }
         }
+
+        if (i%11 == 0) { output = "Bong" }
 
         output = output.ifBlank {i.toString()}
         println(output)
