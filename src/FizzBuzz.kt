3b0e71d1fea6fcfa847d9dd3437626814dd0cfc4
JasCon <jasmin.conterio@softwire.com>
2024-09-12 15:36:11 +0100

    Add Fezz rule

--- src/FizzBuzz.kt
+++ src/FizzBuzz.kt
@@ -3,12 +3,28 @@ fun main() {
         var output = buildString {
             if (i%3 == 0) { append("Fizz") }
             if (i%5 == 0) { append("Buzz") }
-            if (i%5 == 0) { append("Bang") }
+            if (i%7 == 0) { append("Bang") }
         }
-
         if (i%11 == 0) { output = "Bong" }
+        if (i%13 == 0) { output = includeFezz(output) }
 
         output = output.ifBlank {i.toString()}
         println(output)
     }
 }
+
+fun includeFezz(inputString: String): String {
+    if (inputString.isBlank()) {
+        return "Fezz"
+    }
+
+    val sb = StringBuilder(inputString)
+    if (inputString.indexOf("B") != -1) {
+        sb.insert(sb.indexOf("B"), "Fezz")
+    } else {
+        sb.append("Fezz")
+    }
+
+    return sb.toString()
+}
+
